62466aa495c9c6d79606efdc6f0260a53f69d471
Adryan Rizky Alviano <73590164+adryanra97@users.noreply.github.com>
2025-04-23 08:25:05 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from pydantic import BaseSettings, Field
+from dotenv import load_dotenv
+
+class Settings(BaseSettings):
+    OPENAI_CHAT_API_KEY: str = Field(..., env="OPENAI_CHAT_API_KEY")
+    OPENAI_EMBED_API_KEY: str = Field(..., env="OPENAI_EMBED_API_KEY")
+    ELASTICSEARCH_API_KEY: str = Field(..., env="ELASTICSEARCH_API_KEY")
+    TAVILY_API_URL: str = Field(..., env="TAVILY_API_URL")
+    TAVILY_API_KEY: str = Field(..., env="TAVILY_API_KEY")
+    EMBED_OPENAI_API_BASE: str = Field(..., env="EMBED_OPENAI_API_BASE")
+    EMBED_MODEL_NAME: str = Field(..., env="EMBED_MODEL_NAME")
+    ELASTICSEARCH_URL: str = Field(..., env="ELASTICSEARCH_URL")
+    CHAT_OPENAI_API_BASE: str = Field(..., env="CHAT_OPENAI_API_BASE")
+    CHAT_MODEL_NAME: str = Field(..., env="CHAT_MODEL_NAME")
+
+load_dotenv()
+settings = Settings()
